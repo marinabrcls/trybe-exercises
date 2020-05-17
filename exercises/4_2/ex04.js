4e09cf170b685818029f2c0776d2f289fe311a1e
marinabrcls <barcelos.marina@gmail.com>
2020-05-17 00:28:02 -0300

    Add exercice 4

---
+++ exercises/4_2/ex04.js
@@ -0,0 +1,17 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+}
+
+let media = sum / numbers.length;
+
+console.log("Média final: " + media)
+
+if (media > 20) {
+    console.log("Valor maior que 20");
+} else {
+    console.log("Valor menor ou igual a 20")
+}
