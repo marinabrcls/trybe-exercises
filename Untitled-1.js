4d22c329e7667bd433c6cbcc2f2d02fe98c7865a
marinabrcls <barcelos.marina@gmail.com>
2020-05-20 20:27:41 -0300

    Changes on ex 6 and ex 8

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+let texto = "";
+
+for (let n = 0; n < 5; n = n + 1) {
+    texto = "*";
+}
+
+console.log(texto)
+
--- exercises/4_4/ex06.js
+++ exercises/4_4/ex06.js
@@ -1,9 +1,10 @@
 function verificaPalindromo(palavra) {
     let palavraInvertida = "";
-    for (i = palavra.length-1; i >= 0; i -= 1) {
+    for (let i = palavra.length-1; i >= 0; i -= 1) {
         palavraInvertida += palavra[i];
     }
-    if (palavra === palavraInvertida) {
+
+    if (palavra == palavraInvertida) {
         return true;
     } else {
         return false;
